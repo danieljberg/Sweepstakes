69fd9460cf1389003fd97e9676ae4891cf7630cf
unknown <mr.danieljberg@gmail.com>
2019-05-21 15:36:05 -0500

    Init

---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+        //member variables
+
+
+        //constructor
+
+
+        //member methods
+        Sweepstakes(string name)
+        {
+
+        }
+        void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+    }
+}
